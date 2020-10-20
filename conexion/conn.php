6747d33a1d367e5a707712b9d11d09c6467c2a81
leshadeofwolf <68211871+leshadeofwolf@users.noreply.github.com>
2020-10-20 15:45:06 -0700

    AddCoin
    Agregando php para la conexión a la base y la base de datos

---
+++ conexion/conn.php
@@ -0,0 +1,8 @@
+<?php
+
+    $mbd = new PDO('mysql:host=localhost;dbname=standing', 'root', '');
+    /*foreach($mbd->query('SELECT * from citas') as $fila) {
+        print_r($fila);
+    }*/
+
+?>
